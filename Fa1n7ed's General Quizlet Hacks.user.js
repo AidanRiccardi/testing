d21af1781c7f45b9cd0652d80d0a9f3186eaeb07
AidanRiccardi <64086996+AidanRiccardi@users.noreply.github.com>
2020-09-16 09:15:14 -0400

    Add files via upload

---
+++ Fa1n7ed's General Quizlet Hacks.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Fa1n7ed's General Quizlet Hacks
+// @description    Use "v" to activate.
+// @author         Fa1n7ed
+// @include        https://quizlet.com/
+// @version        1.0.4
+// @namespace 
+// ==/UserScript==
+
+(function() {
+var x = document.createElement("script");
+x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/quizlet@master/main.js";
+document.head.appendChild(x);
+})();
